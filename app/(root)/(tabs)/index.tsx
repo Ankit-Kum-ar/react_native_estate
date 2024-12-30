409c0826b209e06d38b9522086e57071271385cd
Ankit-0009 <ankitcode2511@gmail.com>
2024-12-30 16:26:20 +0530

    Add the assets and constants.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="text-3xl py-10 font-rubik">This is good for estate</Text>
       <Link href={'/sign-in'}>SignIn</Link>
       <Link href={'/explore'}>Explore</Link>
       <Link href={'/profile'}>Profile</Link>
---
+++ image.d.ts
@@ -0,0 +1,26 @@
+// Purpose: This file is used to declare the types of image files that are used in the project.
+// The image files are declared as modules so that they can be imported and used in the project.
+// The module declaration is done using the 'declare module' syntax.
+// The module declaration specifies the type of the image file and the default export value of the image file.
+// The module declaration is used to provide type information for the image files that are imported and used in the project.
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+}
+declare module '*.jpg' {
+    const value: any;
+    export default value;
+}
+declare module '*.jpeg' {
+    const value: any;
+    export default value;
+}
+declare module '*.gif' {
+    const value: any;
+    export default value;
+}
+declare module '*.svg' {
+    const value: any;
+    export default value;
+}
